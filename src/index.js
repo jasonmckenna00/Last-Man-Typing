856253327bfab71f9cb062d27c1d8f3ccca53595
Jason McKenna <jasonmckenna00@gmail.com>
2020-01-07 12:03:47 -0500

    made aliens functional component with multiple renderings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import Alien from './scripts/aliens'
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -13,22 +13,52 @@ document.addEventListener("DOMContentLoaded", function () {
     // const game = new Game();
     // new GameView(game, ctx).start();
 
+    
+
+
+    function Alien(){
+        this.size = 30;
+        this.AlienXVel = 1;
+        this.rndPos = Math.floor(Math.random() * 320);
+        
+
+        this.draw = function (){
+            // debugger
+            ctx.beginPath();
+            ctx.rect(0 + this.AlienXVel,this.rndPos,this.size/2,this.size);
+            ctx.fillStyle = '#ff0000';
+            ctx.fill();
+            ctx.closePath();
+            // console.log('asas')
+        }
+
+        this.update = function(){
+            this.AlienXVel += 1;
+            this.draw();
+        }
+
+
+    }
+
     var AlienXVel = 1;
-
-
-    function drawAlien(size){
-        ctx.beginPath();
-        ctx.rect(80 + AlienXVel,80,size,size);
-        ctx.fillStyle = '#ff0000';
-        ctx.fill();
-        ctx.closePath();
+    var alien = new Alien();
+    var alienArray = []
+    for ( let i = 0; i< 4; i++){
+        alienArray.push(new Alien());
     }
 
     function draw(){
+        requestAnimationFrame(draw)
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawAlien(30);
+        for (let i = 0; i < alienArray.length; i++){
+            let delay = Math.floor(Math.random() * 1000);
+            console.log(delay)
+            setTimeout( alienArray[i].update(), delay);
+            
+        }
+        alien.update();
         AlienXVel += 1;
     }
-    setInterval(draw,75);
-
+    // setInterval(draw,75);
+    draw()
   });
---
+++ src/scripts/aliens.js
@@ -0,0 +1,22 @@
+// import { Module } from "module";
+
+class Alien {
+    constructor(ctx){
+    
+    }
+    
+    draw(ctx){
+        let size = 30;
+        let AlienXVel = 1;
+        // rndPos = Math.floor(Math.random() * DIM_Y)
+        let rndPos = Math.floor(Math.random() * 320)
+        ctx.beginPath();
+        ctx.rect(0 + AlienXVel,rndPos,size/2,size);
+        ctx.fillStyle = '#ff0000';
+        ctx.fill();
+        console.log('alien')
+        ctx.closePath();
+    }
+}
+    
+export default Alien;
